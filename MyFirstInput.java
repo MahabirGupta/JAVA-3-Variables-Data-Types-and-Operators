e42cc4d2df42f2bf6e98161872c61f2a45512466
MahabirGupta <89118946+MahabirGupta@users.noreply.github.com>
2021-10-13 22:56:46 +0800

    Add files via upload

---
+++ MyFirstInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class MyFirstInput {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter your name: ");
+            String name = input.nextLine();
+            System.out.println("My name is "+ name);
+        }
+
+    }
+
